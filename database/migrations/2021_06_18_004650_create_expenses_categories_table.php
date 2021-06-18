ccc9b6cb8c1fbdcbc821b3c5c69e73462c62fd77
brunosann <bruno_teixeira20@hotmail.com>
2021-06-17 22:00:25 -0300

    criando as tabelas banco de dados

--- database/migrations/2021_06_18_004650_create_expenses_categories_table.php
+++ database/migrations/2021_06_18_004650_create_expenses_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateExpensesCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('expenses_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->date('birth_date');
-            $table->rememberToken();
+            $table->string('category');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('expenses_categories');
     }
 }
